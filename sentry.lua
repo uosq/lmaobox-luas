e8651db09f28418da7c0792d20c7fc924d8790e2
navet <159667110+uosq@users.noreply.github.com>
2024-07-17 23:14:03 -0300

    Update sentry.lua

--- sentry.lua
+++ sentry.lua
@@ -1,5 +1,5 @@
 local settings = {
-    speed = 1,
+    speed = 1, -- is as low as you can get
     inverted = false,
     real = true, -- if real yaw changes
     fake = true, -- if fake yaw changes
@@ -12,19 +12,30 @@ gui.SetValue("anti aim", 1)
 gui.SetValue("anti aim - yaw (real)", (settings.real == true and "custom" or "none"))
 gui.SetValue("anti aim - yaw (fake)", (settings.fake == true and "custom" or "none"))
 
-gui.SetValue("anti aim - custom yaw (real)", (settings.real == true and 0 or gui.GetValue("anti aim - custom yaw (real)")))
-gui.SetValue("anti aim - custom yaw (fake)", (settings.real == true and 0 or gui.GetValue("anti aim - custom yaw (fake)")))
+local custom_yaw_real = "anti aim - custom yaw (real)"
+local custom_yaw_fake = "anti aim - custom yaw (fake)"
+
+gui.SetValue(custom_yaw_real, (settings.real == true and 0 or gui.GetValue(custom_yaw_real)))
+gui.SetValue(custom_yaw_fake, (settings.fake == true and 0 or gui.GetValue(custom_yaw_fake)))
+
+local yaw = 0
 
 local function change_angle()
-    local yaw = gui.GetValue( "anti aim - custom yaw (fake)" )
-    if (yaw >= 45 or yaw <= -45) then
+    if (yaw >= 45 or yaw <= -45)  then
         direction = -direction
     end
 
     local new_yaw = yaw + (settings.speed * direction * (settings.inverted == true and -1 or 1))
 
-    gui.SetValue( "anti aim - custom yaw (real)", (settings.real == true and new_yaw or 0) )
-    gui.SetValue( "anti aim - custom yaw (fake)", (settings.fake == true and new_yaw or 0) )
+    if settings.real then
+        gui.SetValue(custom_yaw_real, new_yaw)
+    end
+
+    if settings.fake then
+        gui.SetValue(custom_yaw_fake, new_yaw)
+    end
+
+    yaw = new_yaw
 end
 
 callbacks.Register( "CreateMove", change_angle )
