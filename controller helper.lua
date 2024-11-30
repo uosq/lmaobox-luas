f0b977be37b87738e90e897b282580b3cbf608b1
navet <159667110+uosq@users.noreply.github.com>
2024-11-30 15:34:01 -0300

    Update controller helper.lua

--- controller helper.lua
+++ controller helper.lua
@@ -17,11 +17,10 @@ local E_ControllerCodes = {
    RIGHT_STICK_CLICK = 123,
 }
 
-local RIGHT_SHOULDER = 0x2000
-local LEFT_SHOULDER = 0x1000
-
 local left_last_tick = 0
 local right_last_tick = 0
+
+local b_last_tick = 0
 
 callbacks.Register("CreateMove", function (param)
    local state, tick = input.IsButtonPressed(E_ControllerCodes.RIGHT_SHOULDER)
@@ -38,6 +37,15 @@ callbacks.Register("CreateMove", function (param)
       left_last_tick = tick
       local viewangle = engine:GetViewAngles()
       local new_angle = viewangle + Vector3(0,90,0)
+      new_angle = EulerAngles(new_angle.x, new_angle.y, new_angle.z)
+      engine.SetViewAngles(new_angle)
+   end
+
+   local state, tick = input.IsButtonPressed(E_ControllerCodes.B)
+   if state and tick ~= b_last_tick then
+      b_last_tick = tick
+      local viewangle = engine:GetViewAngles()
+      local new_angle = viewangle + Vector3(0,180,0)
       new_angle = EulerAngles(new_angle.x, new_angle.y, new_angle.z)
       engine.SetViewAngles(new_angle)
    end
