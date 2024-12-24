28a73bffe7601876ecba1a04de948f31137dd988
uosq <lboxwallytf2@gmail.com>
2024-12-24 17:51:31 -0300

    add random y to snow

--- snow.lua
+++ snow.lua
@@ -1,6 +1,6 @@
 local only_on_menu = false
 local rainbow_balls = false      --- guaranteed rainbow snowflakes
-local random_rainbow_balls = false
+local random_rainbow_balls = true
 local chance_of_rainbow_ball = 8 --- 1/N, with N = number you replace "8" with
 local num_balls = 500            --- its a good compromise, if you want more i recommend lowering the font size (22 is the default here)
 
@@ -26,8 +26,8 @@ local function create_ball()
    local x = math_random(0, width)
    local y = math_random(-height, 0)
 
-   local white = { 255, 255, 255 }
-   local color = { math_random(0, 255), math_random(0, 255), math_random(0, 255) }
+   local white = { 255, 255, 255, 255 }
+   local color = { math_random(0, 255), math_random(0, 255), math_random(0, 255), 255 }
 
    return math_floor(x), math_floor(y),
        ((math_random(chance_of_rainbow_ball) == 1 and random_rainbow_balls) or rainbow_balls) and color or white
@@ -36,10 +36,6 @@ end
 --- create the balls
 for i = 1, num_balls do
    balls[i] = { create_ball() }
-end
-
-local function time2tick(seconds)
-   return seconds * 66.67
 end
 
 -- Update ball position
@@ -53,7 +49,7 @@ callbacks.Register("CreateMove", function(param)
          balls[i] = { create_ball() }
       end
 
-      balls[i][2] = balls[i][2] + vertical_wind
+      balls[i][2] = balls[i][2] + vertical_wind + math_random(0, 3)
 
       -- Random horizontal movement
       if math_random(1, 32) == 1 then
@@ -70,7 +66,7 @@ callbacks.Register("Draw", function()
    for k, ball in ipairs(balls) do
       local x, y, color = ball[1], ball[2], ball[3]
       if y > 0 and y < height and x > 0 and x < width then
-         Color(color[1], color[2], color[3], 255)
+         Color(color[1], color[2], color[3], color[4])
          Text(x, y, "*")
       end
    end
---
+++ world modulation.lua
@@ -0,0 +1,4 @@
+---@param param DrawModelContext
+callbacks.Register("DrawStaticProps", function(param)
+   param:SetColorModulation(40, 40, 40)
+end)
