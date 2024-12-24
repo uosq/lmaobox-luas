ff77e1de5fac6ef4d878cc3a69beda2a87fcb223
uosq <lboxwallytf2@gmail.com>
2024-12-23 23:16:29 -0300

    add snow

--- crosshair.lua
+++ crosshair.lua
@@ -4,42 +4,59 @@ alib.settings.font = draw.CreateFont("TF2 BUILD", 12, 1000)
 local width, height = draw.GetScreenSize()
 
 local window = {
-    x = 10, y = math.floor(height/2),
-    width = 250, height = 130,
+    x = 10,
+    y = math.floor(height / 2),
+    width = 250,
+    height = 130,
 }
 
 local R_slider = {
-    x = 5, y = 5,
-    width = 240, height = 20,
-    min = 1, max = 255,
+    x = 5,
+    y = 5,
+    width = 240,
+    height = 20,
+    min = 1,
+    max = 255,
     value = 255
 }
 
 local G_slider = {
-    x = 5, y = 30,
-    width = 240, height = 20,
-    min = 1, max = 255,
+    x = 5,
+    y = 30,
+    width = 240,
+    height = 20,
+    min = 1,
+    max = 255,
     value = 255
 }
 
 local B_slider = {
-    x = 5, y = 55,
-    width = 240, height = 20,
-    min = 1, max = 255,
+    x = 5,
+    y = 55,
+    width = 240,
+    height = 20,
+    min = 1,
+    max = 255,
     value = 255
 }
 
 local A_slider = {
-    x = 5, y = 80,
-    width = 240, height = 20,
-    min = 1, max = 255,
+    x = 5,
+    y = 80,
+    width = 240,
+    height = 20,
+    min = 1,
+    max = 255,
     value = 255
 }
 
 local size_slider = {
-    x = 5, y = 105,
-    width = 240, height = 20,
-    min = 2, max = 150,
+    x = 5,
+    y = 105,
+    width = 240,
+    height = 20,
+    min = 2,
+    max = 150,
     value = 6
 }
 
@@ -49,14 +66,14 @@ local getconvar = client.GetConVar
 local setconvar = client.SetConVar
 
 if getconvar("crosshair") ~= 0 then
-	setconvar("crosshair", 0)
+    setconvar("crosshair", 0)
 end
 
-local function draw_crosshair (x, y)
+local function draw_crosshair(x, y)
     local half_size = math.floor(size_slider.value / 2)
     local offset_from_center = 5
 
-    -- Calculate the start and end points for each line 
+    -- Calculate the start and end points for each line
     local top_start_y = y - half_size - offset_from_center
     local top_end_y = y - offset_from_center
 
@@ -70,9 +87,9 @@ local function draw_crosshair (x, y)
     local right_end_x = x + half_size + offset_from_center
 
     -- Draw the lines
-    line(x, top_start_y, x, top_end_y)  -- Top
-    line(left_start_x, y, left_end_x, y) -- Left
-    line(right_start_x, y, right_end_x, y) -- Right
+    line(x, top_start_y, x, top_end_y)       -- Top
+    line(left_start_x, y, left_end_x, y)     -- Left
+    line(right_start_x, y, right_end_x, y)   -- Right
     line(x, bottom_start_y, x, bottom_end_y) -- Bottom
 end
 
@@ -84,9 +101,9 @@ register("Draw", function()
     local me = entities.GetLocalPlayer();
     if not me then return end
     if aimbot.GetAimbotTarget() <= 0 then
-        local source = me:GetAbsOrigin() + me:GetPropVector( "localdata", "m_vecViewOffset[0]" );
+        local source = me:GetAbsOrigin() + me:GetPropVector("localdata", "m_vecViewOffset[0]");
         local destination = source + engine.GetViewAngles():Forward() * 1000
-        local trace = engine.TraceLine( source, destination, MASK_SHOT_HULL );
+        local trace = engine.TraceLine(source, destination, MASK_SHOT_HULL);
 
         local screenPos = client.WorldToScreen(trace.endpos)
         if not screenPos then return end
@@ -104,31 +121,31 @@ register("Draw", function()
     end
 end)
 
-register("CreateMove", function (param)
+register("CreateMove", function(param)
     if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, R_slider) then
         local value = alib.math.GetNewSliderValue(window, R_slider, R_slider.min, R_slider.max)
         R_slider.value = math.floor(value)
-     end
+    end
 
-     if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, G_slider) then
+    if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, G_slider) then
         local value = alib.math.GetNewSliderValue(window, G_slider, G_slider.min, G_slider.max)
         G_slider.value = math.floor(value)
-     end
+    end
 
-     if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, B_slider) then
+    if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, B_slider) then
         local value = alib.math.GetNewSliderValue(window, B_slider, B_slider.min, B_slider.max)
         B_slider.value = math.floor(value)
-     end
+    end
 
-     if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, A_slider) then
+    if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, A_slider) then
         local value = alib.math.GetNewSliderValue(window, A_slider, A_slider.min, A_slider.max)
         A_slider.value = math.floor(value)
-     end
+    end
 
-     if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, size_slider) then
+    if input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) and alib.math.isMouseInside(window, size_slider) then
         local value = alib.math.GetNewSliderValue(window, size_slider, size_slider.min, size_slider.max)
         size_slider.value = math.floor(value)
-     end
+    end
 end)
 
 register("Draw", function()
@@ -136,48 +153,58 @@ register("Draw", function()
     alib.objects.window(window.width, window.height, window.x, window.y, "crosshair settings")
 
     do
-    alib.objects.sliderfade(R_slider.width, R_slider.height, R_slider.x + window.x, R_slider.y + window.y, R_slider.min, R_slider.max, R_slider.value, 255, 50, true)
+        alib.objects.sliderfade(R_slider.width, R_slider.height, R_slider.x + window.x, R_slider.y + window.y,
+            R_slider.min, R_slider.max, R_slider.value, 255, 50, true)
         local percent = (R_slider.value - R_slider.min) / (R_slider.max - R_slider.min)
         draw.SetFont(alib.settings.font)
         local tw, th = draw.GetTextSize(tostring(R_slider.value))
         draw.Color(255, 50, 50, 255)
-        draw.Text(R_slider.x + window.x + math.floor(R_slider.width * percent) - tw, R_slider.y + math.floor(th/2) + window.y, tostring(R_slider.value))
+        draw.Text(R_slider.x + window.x + math.floor(R_slider.width * percent) - tw,
+            R_slider.y + math.floor(th / 2) + window.y, tostring(R_slider.value))
     end
 
     do
-    alib.objects.sliderfade(G_slider.width, G_slider.height, G_slider.x + window.x, G_slider.y + window.y, G_slider.min, G_slider.max, G_slider.value, 255, 50, true)
+        alib.objects.sliderfade(G_slider.width, G_slider.height, G_slider.x + window.x, G_slider.y + window.y,
+            G_slider.min, G_slider.max, G_slider.value, 255, 50, true)
         local percent = (G_slider.value - G_slider.min) / (G_slider.max - G_slider.min)
         draw.SetFont(alib.settings.font)
         local tw, th = draw.GetTextSize(tostring(G_slider.value))
         draw.Color(50, 255, 50, 255)
-        draw.Text(G_slider.x + window.x + math.floor(G_slider.width * percent) - tw, G_slider.y + math.floor(th/2) + window.y, tostring(G_slider.value))
+        draw.Text(G_slider.x + window.x + math.floor(G_slider.width * percent) - tw,
+            G_slider.y + math.floor(th / 2) + window.y, tostring(G_slider.value))
     end
 
     do
-    alib.objects.sliderfade(B_slider.width, B_slider.height, B_slider.x + window.x, B_slider.y + window.y, B_slider.min, B_slider.max, B_slider.value, 255, 50, true)
+        alib.objects.sliderfade(B_slider.width, B_slider.height, B_slider.x + window.x, B_slider.y + window.y,
+            B_slider.min, B_slider.max, B_slider.value, 255, 50, true)
         local percent = (B_slider.value - B_slider.min) / (B_slider.max - B_slider.min)
         draw.SetFont(alib.settings.font)
         local tw, th = draw.GetTextSize(tostring(B_slider.value))
         draw.Color(50, 50, 255, 255)
-        draw.Text(B_slider.x + window.x + math.floor(B_slider.width * percent) - tw, B_slider.y + math.floor(th/2) + window.y, tostring(B_slider.value))
+        draw.Text(B_slider.x + window.x + math.floor(B_slider.width * percent) - tw,
+            B_slider.y + math.floor(th / 2) + window.y, tostring(B_slider.value))
     end
 
     do
-    alib.objects.sliderfade(A_slider.width, A_slider.height, A_slider.x + window.x, A_slider.y + window.y, A_slider.min, A_slider.max, A_slider.value, 255, 50, true)
+        alib.objects.sliderfade(A_slider.width, A_slider.height, A_slider.x + window.x, A_slider.y + window.y,
+            A_slider.min, A_slider.max, A_slider.value, 255, 50, true)
         local percent = (A_slider.value - A_slider.min) / (A_slider.max - A_slider.min)
         draw.SetFont(alib.settings.font)
         local tw, th = draw.GetTextSize(tostring(A_slider.value))
         draw.Color(255, 255, 255, 255)
-        draw.Text(A_slider.x + window.x + math.floor(A_slider.width * percent) - tw, A_slider.y + math.floor(th/2) + window.y, tostring(A_slider.value))
+        draw.Text(A_slider.x + window.x + math.floor(A_slider.width * percent) - tw,
+            A_slider.y + math.floor(th / 2) + window.y, tostring(A_slider.value))
     end
 
     do
-    alib.objects.sliderfade(size_slider.width, size_slider.height, size_slider.x + window.x, size_slider.y + window.y, size_slider.min, size_slider.max, size_slider.value, 255, 50, true)
+        alib.objects.sliderfade(size_slider.width, size_slider.height, size_slider.x + window.x, size_slider.y + window
+        .y, size_slider.min, size_slider.max, size_slider.value, 255, 50, true)
         local percent = (size_slider.value - size_slider.min) / (size_slider.max - size_slider.min)
         draw.SetFont(alib.settings.font)
         local tw, th = draw.GetTextSize(tostring(size_slider.value))
         draw.Color(255, 255, 255, 255)
-        draw.Text(size_slider.x + window.x + math.floor(size_slider.width * percent) - tw, size_slider.y + math.floor(th/2) + window.y, tostring(size_slider.value))
+        draw.Text(size_slider.x + window.x + math.floor(size_slider.width * percent) - tw,
+            size_slider.y + math.floor(th / 2) + window.y, tostring(size_slider.value))
     end
 end)
 
---
+++ snow.lua
@@ -0,0 +1,77 @@
+local only_on_menu = false
+local rainbow_balls = false      --- guaranteed rainbow snowflakes
+local random_rainbow_balls = false
+local chance_of_rainbow_ball = 8 --- 1/N, with N = number you replace "8" with
+local num_balls = 500            --- its a good compromise, if you want more i recommend lowering the font size (22 is the default here)
+
+local balls = {}
+local vertical_wind = 3
+local width, height = draw.GetScreenSize()
+local font = draw.CreateFont("Arial", 22, 1000)
+draw.SetFont(font)
+local tw, th = draw.GetTextSize("*")
+
+--- pre cache these functions, for some reason makes the stuttering stop (or reduce)
+local Text = draw.Text --
+local Color = draw.Color
+local SetFont = draw.SetFont
+local IsMenuOpen = gui.IsMenuOpen
+
+local math_random = math.random
+local math_floor = math.floor
+local math_randomseed = math.randomseed
+local ipairs = ipairs
+
+local function create_ball()
+   local x = math_random(0, width)
+   local y = math_random(-height, 0)
+
+   local white = { 255, 255, 255 }
+   local color = { math_random(0, 255), math_random(0, 255), math_random(0, 255) }
+
+   return math_floor(x), math_floor(y),
+       ((math_random(chance_of_rainbow_ball) == 1 and random_rainbow_balls) or rainbow_balls) and color or white
+end
+
+--- create the balls
+for i = 1, num_balls do
+   balls[i] = { create_ball() }
+end
+
+local function time2tick(seconds)
+   return seconds * 66.67
+end
+
+-- Update ball position
+---@param param UserCmd
+callbacks.Register("CreateMove", function(param)
+   if only_on_menu and not IsMenuOpen() then return end
+   math_randomseed(param.tick_count) --- tick_count is like os.clock
+   for i = 1, num_balls do
+      -- Reset if snowflake goes off screen
+      if balls[i][1] > width or balls[i][1] < 0 or balls[i][2] > height then
+         balls[i] = { create_ball() }
+      end
+
+      balls[i][2] = balls[i][2] + vertical_wind
+
+      -- Random horizontal movement
+      if math_random(1, 32) == 1 then
+         balls[i][1] = balls[i][1] + math_random(-2, 2)
+      end
+   end
+end)
+
+-- Draw snowflakes
+callbacks.Register("Draw", function()
+   if only_on_menu and not IsMenuOpen() then return end
+
+   SetFont(font)
+   for k, ball in ipairs(balls) do
+      local x, y, color = ball[1], ball[2], ball[3]
+      if y > 0 and y < height and x > 0 and x < width then
+         Color(color[1], color[2], color[3], 255)
+         Text(x, y, "*")
+      end
+   end
+end)
