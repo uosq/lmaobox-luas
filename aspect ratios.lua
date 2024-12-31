14c326b602f4f84b5731b6745d9955a70c7e174a
uosq <lboxwallytf2@gmail.com>
2024-12-30 23:20:36 -0300

    added aspect ratios.lua

---
+++ aspect ratios.lua
@@ -0,0 +1,52 @@
+local alib = require("alib")
+alib.settings.font = draw.CreateFont("TF2 BUILD", 12, 1000)
+
+local selected_ratio = 0
+local ratio_index = 0
+
+--- https://www.google.com/search?client=firefox-b-d&q=common+aspect+ratios
+local ratios = { (1 / 1), (3 / 2), (4 / 3), (16 / 9), (5 / 4), (3 / 1), 1.85, 2.40, 0 }
+local ratio_names = { "1:1", "3:2", "4:3", "16:9", "5:4", "3:1",
+   "1.85:1", "2.4:1", "default" }
+
+local window = {}
+window.x = 100
+window.y = 200
+window.width = 200
+window.height = alib.math.GetListHeight(#ratios)
+
+local list = {}
+list.x = 0
+list.y = 0
+list.width = window.width
+list.height = alib.math.GetListHeight(#ratios)
+
+callbacks.Register("CreateMove", function(param)
+   if gui.IsMenuOpen() and not engine.Con_IsVisible() and engine.IsGameUIVisible() and not ((gui.GetValue("clean screenshots") == 1 and engine.IsTakingScreenshot())) then
+      for i, v in ipairs(ratios) do
+         local is_mouse_inside = alib.math.isMouseInside(window, list, "ITEM", i)
+         if is_mouse_inside and input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) then
+            ratio_index = i
+            selected_ratio = v
+         end
+      end
+   end
+end)
+
+callbacks.Register("Draw", function(param)
+   if gui.IsMenuOpen() and not engine.Con_IsVisible() and engine.IsGameUIVisible() and not ((gui.GetValue("clean screenshots") == 1 and engine.IsTakingScreenshot())) then
+      alib.objects.window(window.width, window.height, window.x, window.y, "aspect ratios")
+      alib.objects.list(list.width, list.x + window.x, list.y + window.y, ratio_index, ratio_names)
+   end
+end)
+
+---@param view ViewSetup
+callbacks.Register("RenderView", function(view)
+   if gui.GetValue("clean screenshots") == 1 and engine.IsTakingScreenshot() then
+      view.aspectRatio = 0
+      return
+   end
+   view.aspectRatio = selected_ratio
+end)
+
+callbacks.Register("Unload", alib.unload)
--- custom hud.lua
+++ custom hud.lua
@@ -1,10 +1,10 @@
-local alib = require("source")
+local alib = require("alib")
 alib.settings.window.title.fade.enabled = true
 alib.settings.window.outline.thickness = 0
 alib.settings.checkbox.shadow.offset = 0
 
 local screenw, screenh = draw.GetScreenSize()
-local centerx, centery = math.floor(screenw/2), math.floor(screenh/2)
+local centerx, centery = math.floor(screenw / 2), math.floor(screenh / 2)
 
 local settings = {
 	health = true,
@@ -17,7 +17,8 @@ local settings = {
 }
 
 local window = {
-	x = centerx - 350/2, y = centery - 370/2,
+	x = centerx - 350 / 2,
+	y = centery - 370 / 2,
 	width = 350,
 	height = 370,
 }
@@ -25,53 +26,69 @@ local window = {
 --- i dont know why, i dont want to know why, but my piece of garbage code doesnt detect mouse clicks correctly
 --- if i dont manually make the checkbox
 local health_checkbox = {
-	x = 10, y = 10,
-	width = 50, height = 50,
+	x = 10,
+	y = 10,
+	width = 50,
+	height = 50,
 	checked = settings.health
 }
 
 local ammo_checkbox = {
-	x = 10, y = 70,
-	width = 50, height = 50,
+	x = 10,
+	y = 70,
+	width = 50,
+	height = 50,
 	checked = settings.ammo
 }
 
 local crit_checkbox = {
-	x = 10, y = 130,
-	width = 50, height = 50,
+	x = 10,
+	y = 130,
+	width = 50,
+	height = 50,
 	checked = settings.crit
 }
 
 local warp_checkbox = {
-	x = 10, y = 190,
-	width = 50, height = 50,
+	x = 10,
+	y = 190,
+	width = 50,
+	height = 50,
 	checked = settings.warp
 }
 
 local killfeed_checkbox = {
-	x = 10, y = 250,
-	width = 50, height = 50,
+	x = 10,
+	y = 250,
+	width = 50,
+	height = 50,
 	checked = settings.killfeed
 }
 
 local chat_checkbox = {
-	x = 10, y = 310,
-	width = 50, height = 50,
+	x = 10,
+	y = 310,
+	width = 50,
+	height = 50,
 	checked = settings.chat
 }
 
-local classes = {"scout", "soldier", "pyro", "demoman", "heavyweapons", "engineer", "medic", "sniper", "spy", "random"}
+local classes = { "scout", "soldier", "pyro", "demoman", "heavyweapons", "engineer", "medic", "sniper", "spy", "random" }
 local list = {
-   x = 110, y = 10, width = 180,
-   items = classes,
-   selected_item_index = 1,
+	x = 110,
+	y = 10,
+	width = 180,
+	items = classes,
+	selected_item_index = 1,
 }
 
 --- info panel
 
 local info_window = {
-	x = 5, y = centery - 50/2,
-	width = 120, height = 50
+	x = 5,
+	y = centery - 50 / 2,
+	width = 120,
+	height = 50
 }
 
 --- info panel end
@@ -91,30 +108,17 @@ local should_draw_hud = true
 local TEAM_BLU, TEAM_RED = 3, 2
 
 local color = {
-	[TEAM_BLU] = {102, 255, 255, 255},
-	[TEAM_RED] = {255, 100, 100, 255},
-	[E_TeamNumber.TEAM_SPECTATOR] = {255,255,255,255},
-	[E_TeamNumber.TEAM_UNASSIGNED] = {255,255,255,255}
+	[TEAM_BLU] = { 102, 255, 255, 255 },
+	[TEAM_RED] = { 255, 100, 100, 255 },
+	[E_TeamNumber.TEAM_SPECTATOR] = { 255, 255, 255, 255 },
+	[E_TeamNumber.TEAM_UNASSIGNED] = { 255, 255, 255, 255 }
 }
 
-local last_tick = 0
-callbacks.Register("CreateMove", function ()
-	local state, tick = input.IsButtonPressed(E_ButtonCode.KEY_I)
-	if state and tick ~= last_tick and not engine.IsChatOpen() then
-		last_tick = tick
-		should_draw_hud = not should_draw_hud
-		if not should_draw_hud then
-			client.Command("cl_drawhud 1", true)
-		else
-			client.Command("cl_drawhud 0", true)
-		end
-	end
-
-	if engine.IsChatOpen() and client.GetConVar("cl_drawhud") == 0 then
+callbacks.Register("CreateMove", function()
+	should_draw_hud = not (engine.IsChatOpen() and engine.Con_IsVisible() and not engine.IsGameUIVisible() and client.GetConVar("_cl_classmenuopen") == 1)
+	if not should_draw_hud then
 		client.Command("cl_drawhud 1", true)
-		should_draw_hud = false
-	elseif not engine.IsChatOpen() and client.GetConVar("cl_drawhud") == 1 then
-		should_draw_hud = true
+	else
 		client.Command("cl_drawhud 0", true)
 	end
 end)
@@ -143,12 +147,12 @@ local function hud()
 
 		local team_color = color[localplayer:GetTeamNumber()]
 
-		draw.Color(team_color[1],team_color[2], team_color[3], team_color[4])
+		draw.Color(team_color[1], team_color[2], team_color[3], team_color[4])
 		draw.FilledRect(centerx - width, 0, centerx + width, height)
 
 		draw.Color(255, 255, 255, 255)
 		draw.SetFont(font)
-		draw.TextShadow(centerx - math.floor(textwidth/2), height, str)
+		draw.TextShadow(centerx - math.floor(textwidth / 2), height, str)
 	end
 
 	-- health
@@ -161,11 +165,11 @@ local function hud()
 		healthColor.r = math.floor(255 * (1 - (health / maxhealth)))
 		healthColor.g = math.floor(255 * (health / maxhealth))
 		healthColor.b = 0
-		
+
 		lastHeight = lastHeight + healthSizeY + 20
 		draw.Color(healthColor.r, healthColor.g, healthColor.b, 255)
 		draw.SetFont(font)
-		draw.TextShadow(centerx - math.floor(healthSizeX/2), lastHeight, health_str)
+		draw.TextShadow(centerx - math.floor(healthSizeX / 2), lastHeight, health_str)
 	end
 
 	-- ammo
@@ -178,25 +182,26 @@ local function hud()
 			local clip1 = weapon:GetPropInt("m_iClip1")
 			local ammo_str = ""
 			if clip1 ~= -1 and clip1 ~= nil then
-				ammo_str = isPrimaryWeapon and string.format("%s/%s", clip1, primary_clip2) or string.format("%s/%s", clip1, secondary_clip2)
+				ammo_str = isPrimaryWeapon and string.format("%s/%s", clip1, primary_clip2) or
+					 string.format("%s/%s", clip1, secondary_clip2)
 			else
 				ammo_str = string.format("%s", primary_clip2)
 			end
-	
+
 			local ammoW, ammoH = draw.GetTextSize(ammo_str)
 			draw.SetFont(font)
-			draw.Color(255,255,255,255)
-	
+			draw.Color(255, 255, 255, 255)
+
 			lastHeight = lastHeight + ammoH
-			draw.TextShadow(centerx - math.floor(ammoW/2), lastHeight, ammo_str)
-			else
-				local melee_str = "MELEE"
-				local meleeW, meleeH = draw.GetTextSize(melee_str)
-				draw.SetFont(font)
-				draw.Color(255, 100, 100, 255)
-	
-				lastHeight = lastHeight + meleeH
-				draw.TextShadow(centerx - math.floor(meleeW/2), lastHeight, melee_str)
+			draw.TextShadow(centerx - math.floor(ammoW / 2), lastHeight, ammo_str)
+		else
+			local melee_str = "MELEE"
+			local meleeW, meleeH = draw.GetTextSize(melee_str)
+			draw.SetFont(font)
+			draw.Color(255, 100, 100, 255)
+
+			lastHeight = lastHeight + meleeH
+			draw.TextShadow(centerx - math.floor(meleeW / 2), lastHeight, melee_str)
 		end
 	end
 
@@ -210,7 +215,7 @@ local function hud()
 			draw.SetFont(font)
 			draw.Color(255, 255, 255, 255)
 			lastHeight = lastHeight + metalH
-			draw.TextShadow(centerx - math.floor(metalW/2), lastHeight, metal_str)
+			draw.TextShadow(centerx - math.floor(metalW / 2), lastHeight, metal_str)
 		end
 	end
 
@@ -224,13 +229,14 @@ local function hud()
 		draw.FilledRect(centerx - width, lastHeight, centerx + width, lastHeight + height)
 
 		-- bar
-		local percentage = warp.GetChargedTicks()/23
+		local percentage = warp.GetChargedTicks() / 23
 		if percentage == 1 then
-			draw.Color(150,255,150,255)
+			draw.Color(150, 255, 150, 255)
 		else
-			draw.Color(255,150,150,255)
+			draw.Color(255, 150, 150, 255)
 		end
-		draw.FilledRectFade(centerx - width + 2, lastHeight + 2, math.floor(centerx - width + (width * percentage * 2) - 2), lastHeight + height - 2, 150, 10, false)
+		draw.FilledRectFade(centerx - width + 2, lastHeight + 2, math.floor(centerx - width + (width * percentage * 2) - 2),
+			lastHeight + height - 2, 150, 10, false)
 
 		-- warp text
 		local maxticks = client.GetConVar("sv_maxusrcmdprocessticks") - 1
@@ -238,14 +244,14 @@ local function hud()
 		draw.Color(255, 255, 255, 150)
 		local str = string.format("TICKS: %s/%s", warp.GetChargedTicks(), maxticks)
 		local textwidth, textheight = draw.GetTextSize(str)
-		draw.Text(centerx - math.floor(textwidth/2), lastHeight + 2, str)
+		draw.Text(centerx - math.floor(textwidth / 2), lastHeight + 2, str)
 	end
 
 	-- crit bar
 	if settings.crit then
 		local width, height = 100, 20
 		lastHeight = lastHeight + height + 5
-		
+
 		--- background
 		draw.Color(50, 50, 50, 255)
 		draw.FilledRect(centerx - width, lastHeight, centerx + width, lastHeight + height)
@@ -254,14 +260,15 @@ local function hud()
 		local critbucket = weapon:GetCritTokenBucket()
 		if not critbucket then return end
 
-		local percentage = critbucket/1000 -- 1000 is max
+		local percentage = critbucket / 1000 -- 1000 is max
 		draw.Color(150, 150, 255, 255)
-		draw.FilledRectFade(centerx - width + 2, lastHeight + 2, math.floor(centerx - width + (width * percentage * 2) - 2), lastHeight + height - 2, 150, 10, false)
+		draw.FilledRectFade(centerx - width + 2, lastHeight + 2, math.floor(centerx - width + (width * percentage * 2) - 2),
+			lastHeight + height - 2, 150, 10, false)
 		local str = string.format("CRIT BUCKET: %.1f", weapon:GetCritTokenBucket())
 		local textwidth, textheight = draw.GetTextSize(str)
 		draw.SetFont(warp_font)
 		draw.Color(255, 255, 255, 150)
-		draw.Text(centerx - math.floor(textwidth/2), lastHeight + 2, str)
+		draw.Text(centerx - math.floor(textwidth / 2), lastHeight + 2, str)
 	end
 
 	--- engineer specific (sentry, dispenser, etc)
@@ -310,13 +317,14 @@ local function draw_killfeed()
 	for pos, death in ipairs(killfeed_deaths) do
 		local death_string = ""
 		local died_alone = death.attacker == death.victim
-		
+
 		if death.assister then
-			death_string = string.format("%s + %s x %s", death.attacker:GetName(), death.assister:GetName(), death.victim:GetName())
+			death_string = string.format("%s + %s x %s", death.attacker:GetName(), death.assister:GetName(),
+				death.victim:GetName())
 		else
 			death_string = string.format("%s x %s", death.attacker:GetName(), death.victim:GetName())
 		end
-		
+
 		if died_alone then
 			death_string = string.format("%s died a horrible death :(", death.victim:GetName())
 		end
@@ -362,7 +370,13 @@ local function killfeed(event)
 		local current_tick = globals.TickCount()
 		local hud_deathnotice_time = client.GetConVar("hud_deathnotice_time")
 
-		killfeed_deaths[#killfeed_deaths+1] = {victim = victim, attacker = attacker, assister = assister, tick_to_disappear = current_tick + (hud_deathnotice_time * 66 * 2)}
+		killfeed_deaths[#killfeed_deaths + 1] = {
+			victim = victim,
+			attacker = attacker,
+			assister = assister,
+			tick_to_disappear =
+				 current_tick + (hud_deathnotice_time * 66 * 2)
+		}
 	end
 end
 
@@ -425,7 +439,13 @@ local function chat_msgs(msg)
 		local players = entities.FindByClass("CTFPlayer")
 		for _, player in pairs(players) do
 			if player:GetName() == playerName then
-				chat_messages[#chat_messages+1] = {player = player, message = message, tick_to_disappear = current_tick + (hud_saytext_time * 66), type = chatType}
+				chat_messages[#chat_messages + 1] = {
+					player = player,
+					message = message,
+					tick_to_disappear = current_tick +
+						 (hud_saytext_time * 66),
+					type = chatType
+				}
 			end
 		end
 	end
@@ -440,57 +460,69 @@ local function draw_escape_menu()
 
 	--- i wish i could automate this, but for some reason it doesnt work like it should, but hey at least more control :)
 	--- health
-	alib.objects.checkbox(health_checkbox.width, health_checkbox.height, health_checkbox.x + window.x, health_checkbox.y + window.y, health_checkbox.checked)
+	alib.objects.checkbox(health_checkbox.width, health_checkbox.height, health_checkbox.x + window.x,
+		health_checkbox.y + window.y, health_checkbox.checked)
 	do
 		draw.SetFont(warp_font)
 		local tw, th = draw.GetTextSize("health")
 		draw.Color(255, 255, 255, 255)
-		draw.TextShadow(health_checkbox.x + window.x + health_checkbox.width/2 - math.floor(tw/2), health_checkbox.y + window.y + health_checkbox.height/2 - math.floor(th/2), "health")
+		draw.TextShadow(health_checkbox.x + window.x + health_checkbox.width / 2 - math.floor(tw / 2),
+			health_checkbox.y + window.y + health_checkbox.height / 2 - math.floor(th / 2), "health")
 	end
 
 	--- ammo
-	alib.objects.checkbox(ammo_checkbox.width, ammo_checkbox.height, ammo_checkbox.x + window.x, ammo_checkbox.y + window.y, ammo_checkbox.checked)
+	alib.objects.checkbox(ammo_checkbox.width, ammo_checkbox.height, ammo_checkbox.x + window.x,
+		ammo_checkbox.y + window.y, ammo_checkbox.checked)
 	do
 		draw.SetFont(warp_font)
 		local tw, th = draw.GetTextSize("ammo")
 		draw.Color(255, 255, 255, 255)
-		draw.TextShadow(ammo_checkbox.x + window.x + ammo_checkbox.width/2 - math.floor(tw/2), ammo_checkbox.y + window.y + ammo_checkbox.height/2 - math.floor(th/2), "ammo")
+		draw.TextShadow(ammo_checkbox.x + window.x + ammo_checkbox.width / 2 - math.floor(tw / 2),
+			ammo_checkbox.y + window.y + ammo_checkbox.height / 2 - math.floor(th / 2), "ammo")
 	end
 
 	--- crit
-	alib.objects.checkbox(crit_checkbox.width, crit_checkbox.height, crit_checkbox.x + window.x, crit_checkbox.y + window.y, crit_checkbox.checked)
+	alib.objects.checkbox(crit_checkbox.width, crit_checkbox.height, crit_checkbox.x + window.x,
+		crit_checkbox.y + window.y, crit_checkbox.checked)
 	do
 		draw.SetFont(warp_font)
 		local tw, th = draw.GetTextSize("crit")
 		draw.Color(255, 255, 255, 255)
-		draw.TextShadow(crit_checkbox.x + window.x + crit_checkbox.width/2 - math.floor(tw/2), crit_checkbox.y + window.y + crit_checkbox.height/2 - math.floor(th/2), "crit")
+		draw.TextShadow(crit_checkbox.x + window.x + crit_checkbox.width / 2 - math.floor(tw / 2),
+			crit_checkbox.y + window.y + crit_checkbox.height / 2 - math.floor(th / 2), "crit")
 	end
 
 	--- warp
-	alib.objects.checkbox(warp_checkbox.width, warp_checkbox.height, warp_checkbox.x + window.x, warp_checkbox.y + window.y, warp_checkbox.checked)
+	alib.objects.checkbox(warp_checkbox.width, warp_checkbox.height, warp_checkbox.x + window.x,
+		warp_checkbox.y + window.y, warp_checkbox.checked)
 	do
 		draw.SetFont(warp_font)
 		local tw, th = draw.GetTextSize("warp")
 		draw.Color(255, 255, 255, 255)
-		draw.TextShadow(warp_checkbox.x + window.x + warp_checkbox.width/2 - math.floor(tw/2), warp_checkbox.y + window.y + warp_checkbox.height/2 - math.floor(th/2), "warp")
+		draw.TextShadow(warp_checkbox.x + window.x + warp_checkbox.width / 2 - math.floor(tw / 2),
+			warp_checkbox.y + window.y + warp_checkbox.height / 2 - math.floor(th / 2), "warp")
 	end
 
 	--- killfeed
-	alib.objects.checkbox(killfeed_checkbox.width, killfeed_checkbox.height, killfeed_checkbox.x + window.x, killfeed_checkbox.y + window.y, killfeed_checkbox.checked)
+	alib.objects.checkbox(killfeed_checkbox.width, killfeed_checkbox.height, killfeed_checkbox.x + window.x,
+		killfeed_checkbox.y + window.y, killfeed_checkbox.checked)
 	do
 		draw.SetFont(warp_font)
 		local tw, th = draw.GetTextSize("killfeed")
 		draw.Color(255, 255, 255, 255)
-		draw.TextShadow(killfeed_checkbox.x + window.x + killfeed_checkbox.width/2 - math.floor(tw/2), killfeed_checkbox.y + window.y + killfeed_checkbox.height/2 - math.floor(th/2), "killfeed")
+		draw.TextShadow(killfeed_checkbox.x + window.x + killfeed_checkbox.width / 2 - math.floor(tw / 2),
+			killfeed_checkbox.y + window.y + killfeed_checkbox.height / 2 - math.floor(th / 2), "killfeed")
 	end
 
 	--- chat
-	alib.objects.checkbox(chat_checkbox.width, chat_checkbox.height, chat_checkbox.x + window.x, chat_checkbox.y + window.y, chat_checkbox.checked)
+	alib.objects.checkbox(chat_checkbox.width, chat_checkbox.height, chat_checkbox.x + window.x,
+		chat_checkbox.y + window.y, chat_checkbox.checked)
 	do
 		draw.SetFont(warp_font)
 		local tw, th = draw.GetTextSize("chat")
 		draw.Color(255, 255, 255, 255)
-		draw.TextShadow(chat_checkbox.x + window.x + chat_checkbox.width/2 - math.floor(tw/2), chat_checkbox.y + window.y + chat_checkbox.height/2 - math.floor(th/2), "chat")
+		draw.TextShadow(chat_checkbox.x + window.x + chat_checkbox.width / 2 - math.floor(tw / 2),
+			chat_checkbox.y + window.y + chat_checkbox.height / 2 - math.floor(th / 2), "chat")
 	end
 
 	--- class switcher
@@ -533,7 +565,7 @@ local function mouse_input()
 			settings.chat = chat_checkbox.checked
 		end
 
-		for i,v in ipairs (list.items) do
+		for i, v in ipairs(list.items) do
 			local is_mouse_inside = alib.math.isMouseInsideItem(window, list, i)
 			if is_mouse_inside and input.IsButtonDown(E_ButtonCode.MOUSE_LEFT) then
 				list.selected_item_index = i
@@ -553,12 +585,12 @@ io.write(content)
 io.flush()
 io.close(io.stdout)
 LoadScript("navet custom hud/crosshair.lua")
-callbacks.Register("Unload", function ()
+callbacks.Register("Unload", function()
 	UnloadScript("navet custom hud/crosshair.lua")
 end)
 
 local function hud_manager()
-	if (gui.GetValue("clean screenshots") == 1 and engine.IsTakingScreenshot()) then-- or engine.IsGameUIVisible() or engine.Con_IsVisible() then
+	if (gui.GetValue("clean screenshots") == 1 and engine.IsTakingScreenshot()) then -- or engine.IsGameUIVisible() or engine.Con_IsVisible() then
 		return
 	elseif engine.IsGameUIVisible() and gui.IsMenuOpen() then
 		input.SetMouseInputEnabled(true)
@@ -578,7 +610,7 @@ callbacks.Register("CreateMove", mouse_input)
 callbacks.Register("FireGameEvent", killfeed)
 callbacks.Register("Draw", draw_killfeed)
 
-callbacks.Register("Unload", function ()
+callbacks.Register("Unload", function()
 	client.Command("cl_drawhud 1", true)
 	input.SetMouseInputEnabled(false)
 	gui.SetValue("double tap indicator size", 3)
