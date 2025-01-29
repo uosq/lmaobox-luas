c00534588dc3e6e7b2d1763d31a194f98857f8f3
uosq <lboxwallytf2@gmail.com>
2025-01-29 10:54:44 -0300

    smooth dash - added warning if using fake lag or anti aim, added a few new variables, added clamp function for charged_ticks

--- smooth dash.lua
+++ smooth dash.lua
@@ -10,6 +10,11 @@ local toggle_passive_recharge_key = E_ButtonCode.KEY_R --- Toggles passive recha
 local passive_recharge = true -- if you want to not recharge passively make this false
 local passive_recharge_ticks = 2 --- how much ticks should it recharge every time |  WARNING: DOESNT WORK YET! Waiting for netchannel:SendNetMsg() fix!
 local passive_recharge_time_seconds = 1 --- how often should be recharge passively in seconds?
+
+local shoot_in_recharge = true -- if you try to shoot while recharging it'll stop until you stop shooting (pressing M1 or primary fire key)
+local shoot_in_warp = true -- disable this if you want to stop warping while shooting
+
+local disable_with_projectiles = true -- disables warping when you're holding a projectile weapon and aim method (projectile) is "silent +"
 --- end of settings
 
 --- the charge bar is not mine, i pasted it from another script, idk who tho im sorry :(
@@ -28,18 +33,27 @@ local localplayer = nil
 
 local passive_recharge_time = passive_recharge_time_seconds * 66.67 --- 66.67 is 1 second in source engine spaghetti
 local next_recharge_tick = 0
+local last_pressed_tick = 0
 
 local warping = false
 local recharging = false
 local shooting = false
 
+local isprojectileweapon = false
+
 --- disable tick shifting stuff from lbox
 gui.SetValue("double tap", "none")
 gui.SetValue("dash move key", 0)
 
+-- tbh i dont think we should even get sv_maxusrcmdprocessticks, its not like valve's or community servers will change sv_maxusrcmdprocessticks to something bigger
+-- sticking with the default 24 seems pretty reasonable to me
 local function GetMaxPossibleTicks()
-	local sv_maxusrcmdprocessticks = client.GetConVar("sv_maxusrcmdprocessticks") - 0
+	local sv_maxusrcmdprocessticks = client.GetConVar("sv_maxusrcmdprocessticks") - 0 -- truly the most math of math i have ever mathed, fuck lsp warning about casting any to integer >:(
 	return sv_maxusrcmdprocessticks == 0 and 9999 or sv_maxusrcmdprocessticks -- default is 24 for valve servers
+end
+
+local function clamp(value, min, max)
+	return math.min(math.max(value, min), max)
 end
 
 local maxticks = GetMaxPossibleTicks()
@@ -71,21 +85,22 @@ local function clc_move_tostring(bf)
 	return string.format(str, tostring(m_nNewCommands), tostring(m_nBackupCommands), tostring(m_nLength))
 end
 
-local last_pressed_tick = 0
 ---@param usercmd UserCmd
-callbacks.Register("CreateMove", function(usercmd)
+local function handle_input(usercmd)
 	localplayer = entities:GetLocalPlayer()
 	if not localplayer then
 		return
 	end
 
-	if input.IsButtonDown(send_key) and charged_ticks > 0 and not recharging and not warping then
-		warping = true
+	local weapon = localplayer:GetPropEntity("m_hActiveWeapon")
+	if not weapon then
+		return
 	end
 
-	if input.IsButtonReleased(send_key) and warping then
-		warping = false
-	end
+	isprojectileweapon = not weapon:IsMeleeWeapon()
+		and weapon:GetWeaponProjectileType() ~= E_ProjectileType.TF_PROJECTILE_BULLET
+
+	warping = input.IsButtonDown(send_key) and charged_ticks > 0
 
 	local state, tick = input.IsButtonPressed(toggle_passive_recharge_key)
 	if state and tick ~= last_pressed_tick then
@@ -94,13 +109,15 @@ callbacks.Register("CreateMove", function(usercmd)
 		last_pressed_tick = tick
 	end
 
-	shooting = usercmd.buttons & IN_ATTACK ~= 0
+	shooting = usercmd.buttons & IN_ATTACK ~= 0 and aimbot.GetAimbotTarget() >= 1 -- only works with normal player input, aimbot doesnt change this!
 	maxticks = GetMaxPossibleTicks()
-end)
+	charged_ticks = clamp(charged_ticks, 0, maxticks)
+end
 
 ---@param msg NetMessage
 local function Warp(msg)
-	if msg:GetType() == 6 then
+	if msg:GetType() == 6 then -- SignonState
+		-- E_SignonState.SIGNONSTATE_SPAWN is before we fully join the server, i think its when we are sending or receiving info
 		if clientstate:GetClientSignonState() == E_SignonState.SIGNONSTATE_SPAWN then
 			charged_ticks = 0
 			next_recharge_tick = 0
@@ -117,6 +134,16 @@ local function Warp(msg)
 		and gui.GetValue("fake lag") == 0
 	then
 		if warping and charged_ticks > 0 and not recharging then
+			if
+				(shooting and not shoot_in_warp)
+				or (
+					disable_with_projectiles
+					and isprojectileweapon
+					and gui.GetValue("aim method (projectile)") == "silent +"
+				)
+			then
+				return true
+			end
 			local buffer = create_clc_move_buffer(2, 1)
 			buffer:SetCurBit(6)
 			msg:ReadFromBitBuffer(buffer)
@@ -126,6 +153,12 @@ local function Warp(msg)
 			return true
 		end
 
+		--- early return so we dont recharge
+		if shooting and shoot_in_recharge then
+			return true
+		end
+
+		--- how to make it stop recharging while shooting?
 		if input.IsButtonDown(charge_key) and charged_ticks < maxticks and not warping then
 			recharging = true
 			charged_ticks = charged_ticks + 1
@@ -139,7 +172,6 @@ local function Warp(msg)
 			and charged_ticks < maxticks
 			and globals.TickCount() >= next_recharge_tick
 			and not warping
-			and not shooting
 		then
 			recharging = true
 			charged_ticks = charged_ticks + 1
@@ -168,15 +200,24 @@ local function Draw()
 		barY + barHeight + backgroundOffset
 	)
 
+	local cant_warp = gui.GetValue("anti aim") == 1 or gui.GetValue("fake lag") == 1
+
 	local filledWidth = math.floor((barWidth * used_ticks) / maxticks)
-	if used_ticks == maxticks then
-		draw.Color(1, 221, 103, 255) -- Green
+	if cant_warp then
+		draw.Color(255, 50, 50, 255)
+	elseif used_ticks == maxticks then
+		draw.Color(1, 221, 103, 255) -- green
 	else
-		draw.Color(97, 97, 76, 255) -- Red
+		draw.Color(97, 97, 76, 255) -- darker green
 	end
 	draw.FilledRect(math.floor(barX), math.floor(barY), math.floor(barX + filledWidth), math.floor(barY + barHeight))
 
-	local text = string.format("%i / %i", used_ticks, maxticks)
+	local text
+	if cant_warp then
+		text = "FAKE LAG / ANTIAIM ENABLED"
+	else
+		text = string.format("%i / %i", used_ticks, maxticks)
+	end
 	draw.SetFont(font)
 	local textW, textH = draw.GetTextSize(text)
 	local textX, textY =
@@ -189,3 +230,4 @@ end
 
 callbacks.Register("SendNetMsg", Warp)
 callbacks.Register("Draw", Draw)
+callbacks.Register("CreateMove", handle_input)
