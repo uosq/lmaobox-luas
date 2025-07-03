0bda0c1cb0062b4c14c428a710de60047a5a468b
uosq <159667110+uosq@users.noreply.github.com>
2025-07-03 02:26:56 -0300

    add support for (almost) all materials that need to be changed

--- night mode.lua
+++ night mode.lua
@@ -1,52 +1,90 @@
 --- made by navet
 
---- 1.0 to 0.0, more or less than that and it will simply clamp to 0.0 or 1.0
+--- settings
+
+--- 0 to 100
 local brightness = 10
+
+--- end of settings
+
+local value = brightness / 100
 
 ---@param r number
 ---@param g number
 ---@param b number
----@param sky boolean
-local function apply_color(r, g, b, sky)
+local function apply_color(r, g, b)
 	materials.Enumerate(function(material)
 		local group = material:GetTextureGroupName()
 		local name = material:GetName()
+
 		if
-			(sky and group == "SkyBox textures")
-			or group == "World textures"
+			group == "World textures"
 			or string.find(name, "concrete")
 			or string.find(name, "wood")
 			or string.find(name, "nature")
 			or string.find(name, "wall")
+			or string.find(name, "overlays")
 		then
-			--material:SetShaderParam("$color2", Vector3(r, g, b))
-			material:ColorModulate(r, g, b)
+			material:SetShaderParam("$color2", Vector3(r, g, b))
+		end
+
+		if string.find(name, "props") then
+			if brightness <= 20 then
+				material:SetShaderParam(
+					"$color2",
+					Vector3(((30 + brightness) / 100), ((30 + brightness) / 100), ((30 + brightness) / 100))
+				)
+			else
+				material:SetShaderParam("$color2", Vector3(value, value, value))
+			end
+		end
+	end)
+end
+
+local function unapply_color()
+	materials.Enumerate(function(material)
+		local group = material:GetTextureGroupName()
+		local name = material:GetName()
+		if
+			group == "World textures"
+			or string.find(name, "concrete")
+			or string.find(name, "wood")
+			or string.find(name, "nature")
+			or string.find(name, "wall")
+			or string.find(name, "props")
+			or string.find(name, "overlays")
+		then
+			material:SetShaderParam("$color2", Vector3(1, 1, 1))
 		end
 	end)
 end
 
 local function Prop()
-	render.SetColorModulation(brightness / 100, brightness / 100, brightness / 100)
+	if brightness <= 20 then
+		render.SetColorModulation((30 + brightness) / 100, (30 + brightness) / 100, (30 + brightness) / 100)
+	else
+		render.SetColorModulation(value, value, value)
+	end
 end
 
 ---@param ctx DrawModelContext
 local function DrawModel(ctx)
 	--- does this work 100%? fuck no
 	--- but it should work 80% enough
-	if string.find(ctx:GetModelName(), "prop") then
+	if string.find(ctx:GetModelName(), "prop", 1, true) then
 		if brightness <= 20 then
 			ctx:SetColorModulation((30 + brightness) / 100, (30 + brightness) / 100, (30 + brightness) / 100)
 		else
-			ctx:SetColorModulation(brightness / 100, brightness / 100, brightness / 100)
+			ctx:SetColorModulation(value, value, value)
 		end
 	end
 end
 
 local function Unload()
-	apply_color(1, 1, 1, false)
+	unapply_color()
 end
 
-apply_color(brightness / 100, brightness / 100, brightness / 100, false)
+apply_color(value, value, value)
 
 callbacks.Register("DrawStaticProps", Prop)
 callbacks.Register("DrawModel", DrawModel)
