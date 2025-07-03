991eb578263a7907d7f7d727caba13190c21b71a
uosq <159667110+uosq@users.noreply.github.com>
2025-07-02 21:20:35 -0300

    add target to laggy animations

--- laggy animations.lua
+++ laggy animations.lua
@@ -7,8 +7,10 @@ callbacks.Register("CreateMove", function(cmd)
 		return
 	end
 
-	if (cmd.tick_count % 21) == 0 then
-		plocal:SetPropFloat(globals.CurTime() - (21 * globals.TickInterval()), "m_flAnimTime")
+	local target = (((gui.GetValue("fake lag value (ms)") + 15) / 1000) * 66.67) // 1
+
+	if (cmd.tick_count % target) == 0 then
+		plocal:SetPropFloat(globals.CurTime() - (target * globals.TickInterval()), "m_flAnimTime")
 	else
 		plocal:SetPropFloat(globals.CurTime() + 1, "m_flAnimTime")
 	end
