35e18ec0ab3d51d098cfdae0d9bcdd9b66e71a7d
uosq <159667110+uosq@users.noreply.github.com>
2025-07-02 21:07:40 -0300

    add laggy animations

---
+++ laggy animations.lua
@@ -0,0 +1,15 @@
+--- original by nullptr (goennigoegoe)
+--- modified by navet
+
+callbacks.Register("CreateMove", function(cmd)
+	local plocal = entities.GetLocalPlayer()
+	if not plocal then
+		return
+	end
+
+	if (cmd.tick_count % 22) == 0 then
+		plocal:SetPropFloat(globals.CurTime() - (engine.RandomInt(1, 21) * globals.TickInterval()), "m_flAnimTime")
+	else
+		plocal:SetPropFloat(globals.CurTime() + 1, "m_flAnimTime")
+	end
+end)
