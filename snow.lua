47b2c93b3c69505d1a3438789c311a55b22b0f0a
uosq <lboxwallytf2@gmail.com>
2025-11-04 12:15:47 -0300

    Fix snow only working on a match

--- snow.lua
+++ snow.lua
@@ -8,7 +8,7 @@ local random_rainbow_balls = true
 local chance_of_rainbow_ball = 8 --- 1/N, with N = number you replace "8" with
 local num_balls = 500            --- its a good compromise, if you want more i recommend lowering the font size (22 is the default here)
 
-local vertical_wind = 3
+local vertical_wind = 300
 local width, height = draw.GetScreenSize()
 local font = draw.CreateFont("Arial", 22, 1000)
 
@@ -23,7 +23,6 @@ local IsMenuOpen = gui.IsMenuOpen
 
 local math_random = math.random
 local math_floor = math.floor
-local math_randomseed = math.randomseed
 local ipairs = ipairs
 
 local balls = {}
@@ -44,36 +43,30 @@ for i = 1, num_balls do
    balls[i] = { create_ball() }
 end
 
--- Update ball position
----@param param UserCmd
-callbacks.Register("CreateMove", function(param)
+-- Draw snowflakes
+callbacks.Register("Draw", function()
    if only_on_menu and not IsMenuOpen() then return end
-   math_randomseed(param.tick_count) --- tick_count is like os.clock
+
    for i = 1, num_balls do
       -- Reset if snowflake goes off screen
       if balls[i][1] > width or balls[i][1] < 0 or balls[i][2] > height then
          balls[i] = { create_ball() }
       end
 
-      balls[i][2] = balls[i][2] + vertical_wind + math_random(0, 3)
+      balls[i][2] = balls[i][2] + (vertical_wind + math_random(0, 3)) * globals.FrameTime()
 
       -- Random horizontal movement
       if math_random(1, 32) == 1 then
-         balls[i][1] = balls[i][1] + math_random(-2, 2)
+         balls[i][1] = balls[i][1] + math_random(-2, 2) * globals.FrameTime()
       end
    end
-end)
-
--- Draw snowflakes
-callbacks.Register("Draw", function()
-   if only_on_menu and not IsMenuOpen() then return end
 
    SetFont(font)
    for k, ball in ipairs(balls) do
       local x, y, color = ball[1], ball[2], ball[3]
       if y > 0 and y < height and x > 0 and x < width then
          Color(color[1], color[2], color[3], color[4])
-         Text(x, y, flake)
+         Text(x//1, y//1, flake)
       end
    end
 end)
