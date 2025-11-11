677afb242dead58331443518b35b6ba8f76221b0
uosq <lboxwallytf2@gmail.com>
2025-11-11 16:10:48 -0300

    Improved medieval camera

--- medieval camera.lua
+++ medieval camera.lua
@@ -1,29 +1,40 @@
---- made by navet
---- thirdperson medieval camera
+local oldOrigin = Vector3()
+local targetOrigin = Vector3()
 
+local function Lerp(a, b, t)
+    return a + (b - a) * t
+end
+
+local function VecLerp(a, b, t)
+	local c = Vector3()
+	c.x = Lerp(a.x, b.x, t)
+	c.y = Lerp(a.y, b.y, t)
+	c.z = Lerp(a.z, b.z, t)
+	return c
+end
+
+---@param view ViewSetup
 local function RenderView(view)
-	local plocal = entities.GetLocalPlayer()
-	if plocal == nil then
-		return
+    local plocal = entities.GetLocalPlayer()
+    if not plocal or not plocal:GetPropBool("m_nForceTauntCam") then
+        return
+    end
+
+    local angles = engine.GetViewAngles()
+    local forward, right, up = angles:Forward(), angles:Right(), angles:Up()
+    local origin = view.origin + forward * 12.5 + right * 25.0 + up * -10.0
+
+	local trace = engine.TraceHull(view.origin, origin, Vector3(-10,-10,-10), Vector3(10,10,10), MASK_SHOT_HULL, function() return true end)
+
+    targetOrigin = trace.endpos
+
+	if trace.fraction >= 1.0 then
+		view.origin = targetOrigin
+	else
+		view.origin = Lerp(oldOrigin, targetOrigin, 0.05)
 	end
 
-	--- thirdperson?
-	if plocal:GetPropBool("m_nForceTauntCam") == false then
-		return
-	end
-
-	local forward, right, up, angles
-	angles = engine.GetViewAngles()
-	forward = angles:Forward()
-	right = angles:Right()
-	up = angles:Up()
-
-	local origin = view.origin
-	origin = origin + forward * 12.5
-	origin = origin + right * 25.0
-	origin = origin + up * -10.0
-
-	view.origin = origin
+    oldOrigin = Vector3(view.origin.x, view.origin.y, view.origin.z)
 end
 
 callbacks.Register("RenderView", RenderView)
